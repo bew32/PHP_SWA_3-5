d45b2066dccb78c24e0026fdd3099021c52b332b
bew32 <tawatninja425@gmail.com>
2021-08-09 17:41:06 +0700

    Initial commit

---
+++ person.php
@@ -0,0 +1,24 @@
+
+<?php
+require('./job.php');
+
+class Person{
+  
+  private Object $job;
+
+  public function __construct(){
+    $this->job = new Job();
+    $this->job->setSalary(10000);
+  }
+
+  public function getSalary(): int
+  {
+    return $this->job->getSalary();
+  }
+}
+$person = new Person();
+$person->getSalary();
+
+$salary = $person->getSalary();
+print ($salary);
+?>
